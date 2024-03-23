8d967ac3cbab04f3af3c252b24ce767677003101
Admin <nikolaikoval4uk@yandex.ru>
2024-03-23 19:25:06 +0300

    .

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template("index.html")
+def home():
+    return render_template("index.html", is_active=['true', 'false', 'false'])
+
+@app.route('/courses')
+def courses():
+    return render_template("courses.html", is_active=['false', 'true', 'false'])
+
+@app.route('/settings')
+def settings():
+    return render_template("settings.html", is_active=['false', 'false', 'true'])
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
